d1845b91e61a253538031b70283cd925df87ee1d
TheSwerik <derswerik@gmail.com>
2020-06-03 13:41:48 +0200

    add everything

---
+++ CppCompiler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CppCompiler
+{
+    internal static class CppCompiler
+    {
+        internal static void Main(string[] args) { Console.WriteLine("Hello World!"); }
+    }
+}
